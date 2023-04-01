14e2ee905d3466ce97d685622a0230a27cdad609
FahadSalamatRandhawa <dreamtwister61814@gmail.com>
2023-04-01 04:34:09 -0700

    path 2.0

--- app/Components/Testimonials.tsx
+++ app/Components/Testimonials.tsx
@@ -5,24 +5,8 @@ export default function Testimonials(){
     return (
         <>
             <div className=" flex flex-col justify-center items-center p-3">
-<<<<<<< HEAD
                 <div className="flex justify-center text-3xl mb-10">TestiMo<div className=" text-indigo-400">nials</div></div>
                 <div className=" max-h-[500px] testimonials flex flex-col justify-center flex-nowrap sm:flex sm:flex-row md:flex  md:flex-row touch-pan-x touch-pan-y scroll-smooth snap-mandatory snap-x gap-4 w-3/4 overflow-auto py-5">
-                    <div className=' grid grid-cols-1 col-auto rounded-md shadow-lg shadow-indigo-400 md:w-[300px] md:min-w-[300px] h-[250px] sm:w-[300px] sm:min-w-[300px] snap-start border-indigo-400 border-2 p-2'>
-=======
-                <div className="flex justify-center text-3xl">TestiMo<div className=" text-indigo-400">nials</div></div>
-                <div className=" max-h-[500px] testimonials flex flex-col justify-center flex-nowrap md:flex  md:flex-row touch-pan-x touch-pan-y scroll-smooth snap-mandatory snap-x gap-4 w-3/4 overflow-auto py-5">
-                    <div className=' grid grid-cols-1 col-auto rounded-md shadow-lg shadow-indigo-400 md:w-[300px] md:min-w-[300px] h-[250px] sm:w-[250px] snap-start border-indigo-400 border-2 p-2'>
->>>>>>> 60f534933e8b6ed784eb7a95e807edd5af60cb6b
-                        <div className='flex justify-start'>Upwork</div>
-                        <em className=' flex place-content-center overflow-hidden my-4 indent-8'>"A a asda asd sa  asd asd sa sa dsad sad s sdasdas ad sadsad sad a da  asdsa asd sad sad s asd asd sa sa sad sa d adasd asd sa dsad sad asdasd asd asd asd as dsad ad asd sa dsvery smart and dedicated person who delivered some good quality work sooner than expected"</em>
-                        <div className=' flex h-auto items-end justify-end font-semibold'>Fahad Salamat Randhawa</div>
-                    </div>
-                    <div className=' grid grid-cols-1 col-auto rounded-md shadow-lg shadow-indigo-400 md:w-[300px] md:min-w-[300px] h-[250px] sm:w-[300px] sm:min-w-[300px] snap-start border-indigo-400 border-2 p-2'>
-                        <div className='flex justify-start'>Upwork</div>
-                        <em className=' flex place-content-center overflow-hidden my-4 indent-8'>"A a asda asd sa  asd asd sa sa dsad sad s sdasdas ad sadsad sad a da  asdsa asd sad sad s asd asd sa sa sad sa d adasd asd sa dsad sad asdasd asd asd asd as dsad ad asd sa dsvery smart and dedicated person who delivered some good quality work sooner than expected"</em>
-                        <div className=' flex h-auto items-end justify-end font-semibold'>Fahad Salamat Randhawa</div>
-                    </div>
                     <div className=' grid grid-cols-1 col-auto rounded-md shadow-lg shadow-indigo-400 md:w-[300px] md:min-w-[300px] h-[250px] sm:w-[300px] sm:min-w-[300px] snap-start border-indigo-400 border-2 p-2'>
                         <div className='flex justify-start'>Upwork</div>
                         <em className=' flex place-content-center overflow-hidden my-4 indent-8'>"A a asda asd sa  asd asd sa sa dsad sad s sdasdas ad sadsad sad a da  asdsa asd sad sad s asd asd sa sa sad sa d adasd asd sa dsad sad asdasd asd asd asd as dsad ad asd sa dsvery smart and dedicated person who delivered some good quality work sooner than expected"</em>
